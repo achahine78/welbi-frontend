7891e9ea8d41fcbfb34306d1efe714d6955d8ac7
achahine78 <achahine78@gmail.com>
2023-03-12 18:27:56 +0400

    feat: add missing table columns in residents and programs list

--- src/pages/ProgramsList.tsx
+++ src/pages/ProgramsList.tsx
@@ -1,23 +1,109 @@
-import { Table } from "antd";
+import { Table, Tag } from "antd";
 import { ColumnsType } from "antd/es/table";
+import dayjs from "dayjs";
 import { useContext } from "react";
 import SharedDataContext from "../context/SharedDataContext";
 
-const tableColumns: ColumnsType<object> = [
-  {
-    title: "Name",
-    dataIndex: "name",
-    key: 0,
-    render: (name) => <span>{name}</span>,
-  },
-];
-
 const ProgramsList = () => {
   const { programs } = useContext(SharedDataContext);
+  const tableColumns: ColumnsType<object> = [
+    {
+      title: "Name",
+      dataIndex: "name",
+      render: (name) => <span>{name}</span>,
+    },
+    {
+      title: "Location",
+      dataIndex: "location",
+      render: (location) => <span>{location}</span>,
+    },
+    {
+      title: "All Day",
+      dataIndex: "allDay",
+      width: "5rem",
+      render: (allDay) => <span>{allDay ? "Yes" : "No"}</span>,
+    },
+    {
+      title: "Start Time",
+      dataIndex: "start",
+      width: "6.5rem",
+      render: (start) => <span>{dayjs(start).format("HH:mm")}</span>,
+    },
+    {
+      title: "End Time",
+      dataIndex: "end",
+      width: "6rem",
+      render: (end) => <span>{dayjs(end).format("HH:mm")}</span>,
+    },
+    {
+      title: "Date",
+      dataIndex: "start",
+      render: (start) => <span>{dayjs(start).format("D MMMM, YYYY")}</span>,
+    },
+    {
+      title: "Tags",
+      dataIndex: "tags",
+      render: (tags) => (
+        <div>
+          {tags.map((tag: string) => (
+            <Tag key={tag}>{tag}</Tag>
+          ))}
+        </div>
+      ),
+    },
+    {
+      title: "Dimension",
+      dataIndex: "dimension",
+      render: (dimension) => <span>{dimension}</span>,
+    },
+    {
+      title: "Facilitators",
+      dataIndex: "facilitators",
+      render: (facilitators) => (
+        <div>
+          {facilitators.map((facilitator: string) => (
+            <Tag key={facilitator}>{facilitator}</Tag>
+          ))}
+        </div>
+      ),
+    },
+    {
+      title: "Level of Care",
+      dataIndex: "levelOfCare",
+      render: (levels) => (
+        <div>
+          {levels.map((level: string) => (
+            <Tag key={level}>{level}</Tag>
+          ))}
+        </div>
+      ),
+    },
+    {
+      title: "Hobbies",
+      dataIndex: "hobbies",
+      render: (hobbies) => (
+        <div>
+          {hobbies.map((hobby: string) => (
+            <Tag key={hobby}>{hobby}</Tag>
+          ))}
+        </div>
+      ),
+    },
+    {
+      title: "Recurrence",
+      dataIndex: "recurrence",
+      render: (recurrence) => <span>{String(recurrence) || "N/A"}</span>,
+    },
+    {
+      title: "Repeated",
+      dataIndex: "isRepeated",
+      render: (isRepeated) => <span>{isRepeated ? "Yes" : "No"}</span>,
+    },
+  ];
   console.log("programs: ", programs);
   return (
     <div style={{ width: "100%" }}>
-      <Table dataSource={programs} columns={tableColumns} />
+      <Table dataSource={programs} columns={tableColumns} pagination={false} />
     </div>
   );
 };
--- src/pages/ResidentsList.tsx
+++ src/pages/ResidentsList.tsx
@@ -1,22 +1,56 @@
 import { Table } from "antd";
 import { ColumnsType } from "antd/es/table";
+import dayjs from "dayjs";
 import { useContext } from "react";
 import SharedDataContext from "../context/SharedDataContext";
 
-const tableColumns: ColumnsType<object> = [
-  {
-    title: "Name",
-    dataIndex: "name",
-    render: (name) => <span>{name}</span>,
-  },
-];
-
 const ResidentsList = () => {
   const { residents } = useContext(SharedDataContext);
+  const tableColumns: ColumnsType<object> = [
+    {
+      title: "Name",
+      dataIndex: "name",
+      render: (name) => <span>{name}</span>,
+    },
+    {
+      title: "Preferred Name",
+      dataIndex: "preferredName",
+      render: (preferredName) => <span>{preferredName}</span>,
+    },
+    {
+      title: "Status",
+      dataIndex: "status",
+      render: (status) => <span>{status}</span>,
+    },
+    {
+      title: "Room",
+      dataIndex: "room",
+      render: (room) => <span>{room}</span>,
+    },
+    {
+      title: "Level Of Care",
+      dataIndex: "levelOfCare",
+      render: (levelOfCare) => <span>{levelOfCare}</span>,
+    },
+    {
+      title: "Birthday",
+      dataIndex: "birthDate",
+      render: (birthDate) => (
+        <span>{dayjs(birthDate).format("D MMMM, YYYY")}</span>
+      ),
+    },
+    {
+      title: "Move In Date",
+      dataIndex: "moveInDate",
+      render: (moveInDate) => (
+        <span>{dayjs(moveInDate).format("D MMMM, YYYY")}</span>
+      ),
+    },
+  ];
   console.log("residents: ", residents);
   return (
     <div style={{ width: "100%" }}>
-      <Table dataSource={residents} columns={tableColumns} />
+      <Table dataSource={residents} columns={tableColumns} pagination={false} />
     </div>
   );
 };
