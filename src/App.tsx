3f7dac93b3dde1c318869d8b4aa52c9cd5c4d97a
achahine78 <achahine78@gmail.com>
2023-03-12 17:37:20 +0400

    feat: add residents and programs fetching logic

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,18 @@
 import { Route, Routes } from "react-router-dom";
+import { SharedDataProvider } from "./context/SharedDataContext";
 import AuthGuard from "./guards/AuthGuard";
 
 function App() {
   return (
     <AuthGuard>
       <div className="App">
-        <Routes>
-          <Route path="/" element={<span>base</span>} />
-          <Route path="/residents" element={<span>residents</span>} />
-          <Route path="/programs" element={<span>programs</span>} />
-        </Routes>
+        <SharedDataProvider>
+          <Routes>
+            <Route path="/" element={<span>base</span>} />
+            <Route path="/residents" element={<span>residents</span>} />
+            <Route path="/programs" element={<span>programs</span>} />
+          </Routes>
+        </SharedDataProvider>
       </div>
     </AuthGuard>
   );
---
+++ src/context/SharedDataContext.tsx
@@ -0,0 +1,50 @@
+import React, { useState, ReactNode, useEffect } from "react";
+import { privateAxios } from "../api/api";
+
+const SharedDataContext = React.createContext<{
+  programs: any[];
+  residents: any[];
+  appendResident: (value: any) => null | void;
+  appendProgram: (value: any) => null | void;
+}>({
+  programs: [],
+  residents: [],
+  appendResident: () => null,
+  appendProgram: () => null,
+});
+
+type Props = {
+  children: ReactNode;
+};
+
+const SharedDataProvider = ({ children }: Props) => {
+  const [residents, setResidents] = useState<any>([]);
+  const [programs, setPrograms] = useState<any>([]);
+
+  const appendResident = (newResident: any) =>
+    setResidents([...residents, newResident]);
+  const appendProgram = (newProgram: any) =>
+    setPrograms([...programs, newProgram]);
+
+  useEffect(() => {
+    privateAxios.get("/residents").then(({ data }) => {
+      console.log("residents: ", data);
+      setResidents(data);
+    });
+    privateAxios.get("/programs").then(({ data }) => {
+      console.log("programs: ", data);
+      setPrograms(data);
+    });
+  }, []);
+
+  return (
+    <SharedDataContext.Provider
+      value={{ residents, programs, appendResident, appendProgram }}
+    >
+      {children}
+    </SharedDataContext.Provider>
+  );
+};
+
+export default SharedDataContext;
+export { SharedDataProvider };
